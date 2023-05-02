8889e86161cdf0bb99ea702b6bb68cbf12cca820
Jahid Hasan <jh3495689@gmail.com>
2023-05-03 04:39:57 +0600

    chef and racipe id add and complete to server site data

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 const port = process.env.port || 5000
 
 
-const chefInformation = require('./chef.json')
+const chefInformation = require('./data/chef.json')
+const racipe = require('./data/recipes.json')
 
 
 app.use(cors())
@@ -17,10 +18,20 @@ app.get('/chef', (req, res)=> {
     res.send(chefInformation)
 })
 
+app.get('/racipe', (req, res) => {
+    res.send(racipe)
+})
+
 app.get('/chef/:id', (req, res) => {
     const id = req.params.id
     const chefRacipe = chefInformation.find(racipe => racipe.id == id)
     res.send(chefRacipe)
+})
+
+app.get('/racipe/:id', (req, res) => {
+    const id = req.params.id
+    const racipeItems = racipe.filter(item => item.id === id)
+    res.send(racipeItems)
 })
 
 app.listen(port, () => {
