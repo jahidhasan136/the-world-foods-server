4f55349887fe3c2190ab09bcfb9fbc92fee879e1
Jahid Hasan <jh3495689@gmail.com>
2023-05-02 22:53:10 +0600

    equal mistake to fix server site data load

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.get('/chef', (req, res)=> {
     res.send(chefInformation)
 })
 
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id
+    const chefRacipe = chefInformation.find(racipe => racipe.id == id)
+    res.send(chefRacipe)
+})
+
 app.listen(port, () => {
     console.log(port)
 })
