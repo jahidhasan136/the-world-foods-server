4fc59c1f360f05d7936c95527017f4562552442c
Jahid Hasan <jh3495689@gmail.com>
2023-05-04 13:41:15 +0600

    modify chef json code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = process.env.port || 5000
 
 
 const chefInformation = require('./data/chef.json')
-const racipe = require('./data/recipes.json')
 
 
 app.use(cors())
@@ -18,20 +17,10 @@ app.get('/chef', (req, res)=> {
     res.send(chefInformation)
 })
 
-app.get('/racipe', (req, res) => {
-    res.send(racipe)
-})
-
 app.get('/chef/:id', (req, res) => {
     const id = req.params.id
     const chefRacipe = chefInformation.find(racipe => racipe.id == id)
     res.send(chefRacipe)
-})
-
-app.get('/racipe/:id', (req, res) => {
-    const id = req.params.id
-    const racipeItems = racipe.filter(item => item.id === id)
-    res.send(racipeItems)
 })
 
 app.listen(port, () => {
